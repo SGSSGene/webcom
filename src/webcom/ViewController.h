3fb8230b9cf77709ad7d4712c36b39303e4e0b66
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-11-07 14:40:14 +0100

    [patch] add some docu to ViewController

--- src/webcom/ViewController.h
+++ src/webcom/ViewController.h
@@ -14,6 +14,19 @@ struct ViewController {
     GetSize&  getBufferedAmount;
     Service&  service;
 
+    ViewController() = delete;
+    ViewController(ViewController const&) = delete;
+    ViewController(ViewController&&) = default;
+
+    ViewController(SendData& _sendData, GetSize& _getSize, Service& _service)
+        : sendData{_sendData}
+        , getBufferedAmount{_getSize}
+        , service{_service}
+    {}
+    auto operator=(ViewController const&) -> ViewController = delete;
+    auto operator=(ViewController&&) -> ViewController = delete;
+
+
     struct Call {
         enum class Type {All, Back, Others};
         Type type;
@@ -24,12 +37,24 @@ struct ViewController {
         void operator()(Args&&... _args) const;
     };
 
+    /**
+     * Call funciton _actionName on all remote peers
+     */
     auto callAll(std::string_view _actionName) const {
         return Call{Call::Type::All, *this, _actionName};
     }
+
+    /**
+     * Call function _actionName on the client associated with this ViewController
+     */
     auto callBack(std::string_view _actionName) const {
         return Call{Call::Type::Back, *this, _actionName};
     }
+
+    /**
+     * Call function _actionName on all remote peers, but excluding the client
+     * associated with this ViewController
+     */
     auto callOthers(std::string_view _actionName) const {
         return Call{Call::Type::Others, *this, _actionName};
     }
@@ -38,8 +63,6 @@ struct ViewController {
     auto make(Args&&... args) {
         return std::make_shared<T>(std::forward<Args>(args)...);
     }
-
-    operator bool() const;
 };
 }
 #include "Service.h"
@@ -62,10 +85,6 @@ void ViewController::Call::operator()(Args&&... _args) const {
             }
         }
     }
-}
-
-ViewController::operator bool() const {
-    return service;
 }
 
 }

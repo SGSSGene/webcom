bca2a80006bcaf0a4a998d3313515ca8610703aa
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-11-26 11:08:30 +0100

    [fix] make webcom::Services multithread safe

--- src/webcom/Services.h
+++ src/webcom/Services.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "GuardedType.h"
 #include "Service.h"
 #include "ViewController.h"
 
@@ -11,23 +12,32 @@ namespace webcom {
 template <typename T>
 class Services {
 private:
-    std::unordered_map<std::string, ServiceT<T>> serviceList;
+    GuardedType<std::unordered_map<std::string, ServiceT<T>>> serviceList;
 public:
 
     template <typename CB>
     auto provideViewController(std::string const& _key, CB cb) -> auto& {
-        auto [iter, succ] = serviceList.try_emplace(_key, cb);
+        auto&& [guard, list] = *serviceList;
+
+        auto [iter, succ] = list.try_emplace(_key, cb);
         return iter->second;
     }
 
     template <typename CB>
     auto subscribe(std::string_view _serviceName, CB cb, T t) -> std::unique_ptr<ViewController> {
-        return getService(_serviceName).createViewController(std::move(cb), std::move(t));
+        auto&& [guard, list] = *serviceList;
+
+        auto iter = list.find(std::string{_serviceName}); //!TODO how to make it work with std::string_view
+        if (iter == end(list)) {
+            throw std::runtime_error(fmt::format("unknown service \"{}\"", _serviceName));
+        }
+        return iter->second.createViewController(std::move(cb), std::move(t));
     }
 
     auto getService(std::string_view _key) -> ServiceT<T>& {
-        auto iter = serviceList.find(std::string{_key}); //!TODO how to make it work with std::string_view
-        if (iter == end(serviceList)) {
+        auto&& [guard, list] = *serviceList;
+        auto iter = list.find(std::string{_key}); //!TODO how to make it work with std::string_view
+        if (iter == end(list)) {
             throw std::runtime_error(fmt::format("unknown service \"{}\"", _key));
         }
         return iter->second;
